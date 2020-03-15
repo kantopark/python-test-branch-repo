6f1f8b2a569e9e1d02a8ea30ccc651146d28b130
Daniel Bok <daniel.bok@outlook.com>
2020-03-15 23:21:50 +0800

    Add error message printed to stderr

--- step_1_1.py
+++ step_1_1.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     code = int(getenv("exit_code_1_1", "0"))
     print("%s has exit code: %d" % (sys.argv[0], code))
 
+    if code != 0:
+        print("An error message printed to std error", file=sys.stderr)
     exit(code)
