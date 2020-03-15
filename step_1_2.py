969e683a51ee22b173a215ad7f4b8667c03df787
Daniel Bok <daniel.bok@outlook.com>
2020-03-15 21:28:53 +0800

    More granular exit_code environment variable for better testing

--- step_1_2.py
+++ step_1_2.py
@@ -2,7 +2,7 @@ import sys
 from os import getenv
 
 if __name__ == '__main__':
-    code = int(getenv("exit_code", "0"))
+    code = int(getenv("exit_code_1_2", "0"))
     print("%s has exit code: %d" % (sys.argv[0], code))
 
     exit(code)
